ee89e45dfb3322400a1a0fa3a4d75976205e5449
codebendr-product-designer <codebendr@gmail.com>
2021-09-20 10:16:44 +0000

    added the network module to the workspace.

---
+++ MainApp/MainApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainApp
+//
+//  Created by Evans Domina Attafuah on 20/09/2021.
+//
+
+import UIKit
+import NetworkModule
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
