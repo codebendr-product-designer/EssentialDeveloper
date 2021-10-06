646228bdbccc82d91aa3a8d577cd9b24bf4de921
codebendr-product-designer <codebendr@gmail.com>
2021-10-06 17:58:56 +0000

    removed default value for completion block (moved it to test)

--- NetworkModule/Sources/NetworkModule/FeedAPI/RemoteFeedLoader.swift
+++ NetworkModule/Sources/NetworkModule/FeedAPI/RemoteFeedLoader.swift
@@ -22,7 +22,7 @@ public final class RemoteFeedLoader {
         self.url = url
     }
     
-    public func load(completion: @escaping (Error) -> () = ) {
+    public func load(completion: @escaping (Error) -> ()) {
         client.get(from: url) { error in
             completion(.connectivity)
         }
