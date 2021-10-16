97d4cf3543bd65621efd7ea0e2f506d00e18f94e
codebendr-product-designer <codebendr@gmail.com>
2021-10-16 08:50:26 +0000

    testing delivers failure with error on url request

--- NetworkModule/Tests/NetworkModuleTests/URLSessionHTTPClientTests.swift
+++ NetworkModule/Tests/NetworkModuleTests/URLSessionHTTPClientTests.swift
@@ -1,4 +1,6 @@
 import XCTest
+import NetworkModule
+
 
 class URLSessionHTTPClient {
     private let session: URLSession
@@ -7,50 +9,86 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
-    func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in}.resume()
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.recievedURLs, [url])
-    }
+//    func test_getFromURL_createsDataTaskWithURL() {
+//        let url = URL(string: "http://any-url.com")!
+//        let session = URLSessionSpy()
+//        let sut = URLSessionHTTPClient(session: session)
+//        sut.get(from: url)
+//
+//        XCTAssertEqual(session.recievedURLs, [url])
+//    }
     
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
         let sut = URLSessionHTTPClient(session: session)
-        sut.get(from: url)
+        sut.get(from: url) { _ in }
         
         XCTAssertEqual(session.recievedURLs, [url])
+    }
+    
+    func test_getFromURLfailsOnRequestError() {
+        let url = URL(string: "http://any-url.com")!
+        let error = NSError(domain: "any error", code: 1)
+        let session = URLSessionSpy()
+        
+        let sut = URLSessionHTTPClient(session: session)
+         
+        let exp = expectation(description: "Wait for completion")
+        
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(receivedError as NSError) :
+                XCTAssertEqual(receivedError.domain, error.domain)
+            default:
+                XCTFail("Expected failure with error \(error), got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        XCTAssertEqual(session.recievedURLs, [url])
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     //MARK: - Helpers
     private class URLSessionSpy: URLSession {
         var recievedURLs = [URL]()
-        private var stubs = [URL: URLSessionDataTask]()
+        private var stubs = [URL: Stub]()
         
-        func stub(url: URL, task: URLSessionDataTask) {
-            stubs[url] = task
+        private struct Stub {
+            let task: URLSessionDataTask
+            let error: Error?
         }
         
-        override func dataTask(with url: URL) -> URLSessionDataTask {
-            recievedURLs.append(url)
-            return stubs[url] ?? FakeURLSessionDataTask()
+        func stub(url: URL, task: URLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
+            stubs[url] = Stub(task: task, error: error)
+        }
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) -> URLSessionDataTask {
+            guard let stub = stubs[url] else {
+               fatalError("Couldn't find stub for \(url)")
+            }
+            
+            completionHandler(nil, nil, stub.error)
+            return stub.task
         }
     }
     
     private class FakeURLSessionDataTask: URLSessionDataTask {
-        override func resume() {
-        
-        }
+        override func resume() { }
     }
     
     private class URLSessionDataTaskSpy: URLSessionDataTask {
