cf43f52622df1cdebdde05efd8b1ab89f7c7b249
codebendr-product-designer <codebendr@gmail.com>
2021-10-23 07:22:19 +0000

    intercepted setup and teardown.

--- NetworkModule/Tests/NetworkModuleTests/URLSessionHTTPClientTests.swift
+++ NetworkModule/Tests/NetworkModuleTests/URLSessionHTTPClientTests.swift
@@ -21,8 +21,18 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_performsGETRequestWithURL() {
+    override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    func test_getFromURL_performsGETRequestWithURL() {
+  
         
         let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for request")
@@ -35,11 +45,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLSessionHTTPClient().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
+
     }
     
     func test_getFromURLfailsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
+       // URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
@@ -60,7 +70,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
+      //  URLProtocolStub.stopInterceptingRequests()
     }
     
     //MARK: - Helpers
@@ -87,6 +97,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         static func stopInterceptingRequests() {
             Self.unregisterClass(Self.self)
+            requestObserver = nil
             stub = nil
         }
         
